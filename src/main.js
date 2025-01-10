46c6424334c2290153292f304f0ad26a4eaf3ca4
邢浩东 <975115611@qq.com>
2025-01-10 17:25:44 +0800

    feat(menu): 添加菜单权限控制并优化用户界面
    - 在 menuStore 中添加按钮权限数组 btnPermsArry
    - 在登录时获取用户权限并存储到菜单仓库
    - 在内部用户和菜单资源页面添加权限控制指令 v-has-permi
    - 优化首页布局，添加用户头像下拉菜单
    - 删除产品页面
    - 修复菜单图标弹窗关闭逻辑

--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@ import axios from './utils/request';
 import pinia from './stortes'; //引入
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import zhCn from 'element-plus/es/locale/lang/zh-cn'; // 引入中文语言包
+import { checkPermissions } from './utils/permissions';
+import hasPermi from './directives/hasPermi'; // 引入自定义指令
 // import '@/assets/css/tailwind.css';
 import 'uno.css';
 const app = createApp(App);
 app.config.globalProperties.$api = axios;
 app.config.globalProperties.$icon = ElementPlusIconsVue;
+// 引入权限校验方法
+app.config.globalProperties.$checkPermissions = checkPermissions;
+// 注册全局指令 v-has-permi
+app.directive('has-permi', hasPermi);
 // size: 'small',
 app.use(router).use(pinia).use(ElementPlus, { locale: zhCn, zIndex: 3000 });
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
--- src/pages/systemSetting/internalusers.vue
+++ src/pages/systemSetting/internalusers.vue
@@ -3,6 +3,7 @@
         <!--用户管理 -->
         <div class="mb-4">
             <el-button
+                v-has-permi="['add:user']"
                 type="primary"
                 @click="
                     dialogVisible = true;
@@ -47,8 +48,8 @@
             <!-- 操作 -->
             <el-table-column label="操作" width="130" fixed="right">
                 <template #default="scope">
-                    <el-button link type="primary" @click="handleEdit(scope.row)">编辑</el-button>
-                    <el-button link type="primary" @click="handleDelete(scope.row)">删除</el-button>
+                    <el-button  v-has-permi="['add:user']" link type="primary" @click="handleEdit(scope.row)">编辑</el-button>
+                    <el-button  v-has-permi="['add:user']" link type="primary" @click="handleDelete(scope.row)">删除</el-button>
                     <!-- <el-button link type="primary" @click="handleRelieve(scope.row)" v-text="scope.row.is_active == 0 ? '解除' : '禁用'"></el-button> -->
                 </template>
             </el-table-column>
