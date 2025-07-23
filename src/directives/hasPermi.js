c0fd0a079880b740958fa6355918aefaebc113cb
xinghaodong <975115611@qq.com>
2025-07-23 22:56:10 +0800

    test(env): 更新测试环境配置并修复权限指令
    - 修改测试环境后端地址为 http://localhost:8888/api
    - 修复 hasPermi 指令中的权限判断逻辑

--- src/directives/hasPermi.js
+++ src/directives/hasPermi.js
@@ -6,17 +6,18 @@ export default {
         const permissions = useMenuStoresdata.btnPermsArry;
         const requiredPermissions = binding.value; // 获取传递的权限数组
         const all_permission = '*:*:*'; // 假设有一个表示所有权限的值
-
         // 判断是否拥有权限
-        const hasPermission = permissions.some(permission => {
-            return all_permission === permission || requiredPermissions.includes(permission);
-        });
+        if (permissions) {
+            const hasPermission = permissions.some(permission => {
+                return all_permission === permission || requiredPermissions.includes(permission);
+            });
 
-        // 如果没有权限，隐藏元素
-        if (!hasPermission) {
-            el.style.display = 'none';
-        } else {
-            el.style.display = ''; // 如果有权限，显示元素
+            // 如果没有权限，隐藏元素
+            if (!hasPermission) {
+                el.style.display = 'none';
+            } else {
+                el.style.display = ''; // 如果有权限，显示元素
+            }
         }
     },
 };
